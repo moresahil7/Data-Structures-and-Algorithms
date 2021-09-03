f0bf118bf1589bd42e50d2c43baef281a45ebb53
Sahil <moresahil4502@gmail.com>
2021-09-03 17:03:12 +0530

    increasing and decreasing order of numbers

---
+++ Recursion/increasingorder.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+void inc(int n){
+
+    if(n == 0){
+        return;
+    }
+    inc(n-1);
+    cout<<n;
+
+
+
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    inc(n);
+}
