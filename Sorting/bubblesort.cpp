a4f3385b9ec9ddcd4f096e242414f221cec8090d
Sahil <moresahil4502@gmail.com>
2021-09-09 17:36:38 +0530

    modified files

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -1,41 +1,35 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
-    int n;
-    cout<<"Enter the number of elements to be entered in an array"<<endl;
-    cin>>n;
-    int arr[n];
 
+int main() {
+
+
+    int n;
+    cin>>n;
+
+    int arr[n];
 
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Elements in an array are"<<endl;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
     cout<<endl;
-for(int i=0;i<n;i++){
-    for(int j=0;j<n-i;j++){
-        
-        if(arr[j]>arr[j+1]){
-            int temp = arr[j+1];
-            arr[j+1] = arr[j];
-            arr[j] = temp;
-        }
-            
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
     }
-    
-}
-    cout<<"Sorted elements are"<<endl;
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-
-
-
 }
--- Sorting/insertionsort.cpp
+++ Sorting/insertionsort.cpp
@@ -1,45 +1,39 @@
 #include<iostream>
-
 using namespace std;
 
+
+
 int main(){
+
+
     int n;
-    cout<<"Enter the number of elements to be entered in an array"<<endl;
     cin>>n;
     int arr[n];
 
-cout<<"Enter elements in an array"<<endl;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Elements in an array are"<<endl;
+
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i];
     }
-    cout<<endl;
-    for(int i=1; i<n ; i++){
+cout<<endl;
+
+    for(int i = 1; i<n;i++){
+
         int current = arr[i];
-        int j = i-1;
-        while( arr[j]>current && j>=0 ){
+        int j= i-1;
+        while(arr[j]>current && j>=0){
             arr[j+1] = arr[j];
             j--;
         }
         arr[j+1] = current;
-        current++;
-       
     }
-  
- 
+cout<<endl;
 
-
-
-
-
-
-    cout<<"Sorted elements are"<<endl;
 
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i];
     }
 
 
