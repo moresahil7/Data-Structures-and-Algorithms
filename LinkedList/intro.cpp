eabd9a43e10d9abda0df91383727c27d47602f14
Sahil <moresahil4502@gmail.com>
2021-10-06 13:17:59 +0530

    delettion operation

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -7,81 +7,92 @@ class node{
     int data;
     node* link;
 
+
     node(int val){
         data = val;
         link = NULL;
     }
-
-
 };
 
-void insertAtHead(node* &head, int val){
 
+void insertAtHead(node* &head, int val){
     node* n = new node(val);
+
     n->link = head;
     head = n;
-
 }
 
-
 void insertAtTail(node* &head, int val){
-    node* n =  new node(val);
+    node* n =new node(val);
 
     if(head == NULL){
         head = n;
         return;
     }
 
+
     node* temp = head;
-
-
     while(temp->link != NULL){
         temp=temp->link;
     }
+
     temp->link = n;
+
 }
 
-void display(node* head){
-     
-
-     while(head != NULL){
-         cout<<head->data<<"->";
-         head = head->link;
-     }
-     cout<<endl;
-}
-
-bool search(node* head, int key){
 
 
-    while(head != NULL){
+bool Search(node* head, int key){
+    while(head != 0){
         if(head->data == key){
             return true;
 
         }
         head = head->link;
-
     }
     return false;
 }
 
+
+void deletion(node* &head , int val){
+
+    node* temp = head;
+
+    while(temp->link->data != val){
+
+        temp=temp->link;
+
+    }
+
+    node* toDelete = temp->link;
+
+
+    temp->link = temp->link->link;
+    delete toDelete;
+
+}
+
+void display(node* head){
+     while(head != NULL){
+         cout<<head->data<<"->";
+         head = head->link;
+     }
+     cout<<"NULL"<<endl;
+}
+
 int main(){
-    node* head =NULL;
+    node* head = NULL;
 
+    insertAtTail(head, 12);
+    insertAtTail(head, 13);
+    insertAtTail(head, 14);
 
-    insertAtTail(head , 23);
-    insertAtTail(head , 24);
-    insertAtTail(head , 25);
-    insertAtTail(head , 26);
-    insertAtTail(head , 27);
+    insertAtHead(head, 15);
+    insertAtHead(head, 18);
+
+    deletion(head,12);
+
     display(head);
 
-
-
-    insertAtHead(head, 10);
-    display(head);
-
-    cout<<search(head, 23);
-
-
+    cout<<Search(head, 18);
 }
--- LinkedList/intro1.cpp
+++ LinkedList/intro1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class node{
     public:
+
     int data;
     node* link;
 
@@ -11,66 +12,92 @@ class node{
         data = val;
         link = NULL;
     }
+
 };
+void insertAtHead(node* &head, int val){
+    node* n= new node(val);
 
-void insertAtHead(node* &head , int val){
-    
-    node* n =new node(val);
-
-    n->link = head;   //link of n points towards head
-    head = n;         
+    n->link = head;
+    head = n;
 }
 
+void insertAtTail(node* &head , int val){
+    node* n = new node(val);
 
- //node* &head:-passes pointer to the head by reference so we can change head
-void insertAtTail(node* &head, int val) {           
-    node* n = new node(val);   //datafield of n will contain val and link will be null
-
-    if(head == NULL){
-        head = n;
+    if(head ==NULL){
+        head =n;
         return;
     }
+
     node* temp = head;
 
     while(temp->link !=NULL){
         temp=temp->link;
-    } 
-    temp->link = n;                 //value added at last of the linked list // temps link points towards n
-}  
+    }
+
+    temp->link = n;
+}
 
 
+bool search(node* head, int key){
 
-bool Search(node* head, int key){
-    
     while(head != NULL){
         if(head->data == key){
             return true;
         }
-        head= head->link;
+        head = head->link;
     }
+    return false;
+}
+
+
+void deletion(node* &head, int val){
+    node* temp =head;
+
+    while(temp->link->data != val){
+        temp=temp->link;
+    }
+
+    node* toDelete = temp->link;
+
+
+    temp->link = temp->link->link;
+
+    delete toDelete;
 }
 
 void display(node* head){
 
-    while(head != NULL){
+
+    while(head !=NULL){
         cout<<head->data<<"->";
         head = head->link;
     }
-    cout<<endl;
+    cout<<"NULL"<<endl;
 }
 
-int main(){
+
+int main() {
     node* head = NULL;
 
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
-    insertAtTail(head, 4);
-    insertAtTail(head, 5);
+
+    insertAtTail(head , 1);
+    insertAtTail(head , 2);
+    insertAtTail(head , 3);
+    insertAtTail(head , 4);
+
+    insertAtHead(head, 0);
+
+
+
+    deletion(head , 4);
+
+
+
+
 
     display(head);
-    insertAtHead(head, 10);
-    display(head);
+    cout<<search(head , 1)<<endl;
 
-    cout<<Search(head,2);
 }
+
