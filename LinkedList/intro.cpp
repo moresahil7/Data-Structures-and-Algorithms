da8cc7b32be748a39ce47bc3a4ee98e1615e111a
Sahil <moresahil4502@gmail.com>
2021-10-06 10:18:54 +0530

    modified files

--- LinkedList/intro.cpp
+++ LinkedList/intro.cpp
@@ -11,63 +11,77 @@ class node{
         data = val;
         link = NULL;
     }
+
+
 };
 
 void insertAtHead(node* &head, int val){
 
     node* n = new node(val);
     n->link = head;
-    head=n;
+    head = n;
+
 }
 
+
 void insertAtTail(node* &head, int val){
-    node* n = new node(val);
+    node* n =  new node(val);
 
     if(head == NULL){
         head = n;
         return;
     }
 
-
     node* temp = head;
 
+
     while(temp->link != NULL){
-        temp = temp->link;
+        temp=temp->link;
     }
     temp->link = n;
 }
 
-bool Search(node* head, int key){
+void display(node* head){
+     
+
+     while(head != NULL){
+         cout<<head->data<<"->";
+         head = head->link;
+     }
+     cout<<endl;
+}
+
+bool search(node* head, int key){
+
 
     while(head != NULL){
         if(head->data == key){
             return true;
+
         }
-        head=head->link;
+        head = head->link;
+
     }
     return false;
 }
 
-
-void display(node* head){
-    while(head != NULL){
-        cout<<head->data<<"->";
-        head = head->link;
-    }
-
-    cout<<"NULL"<<endl;
-}
-
 int main(){
-    node* head = NULL;
-    insertAtTail(head, 1);
-    insertAtTail(head, 2);
-    insertAtTail(head, 3);
+    node* head =NULL;
 
+
+    insertAtTail(head , 23);
+    insertAtTail(head , 24);
+    insertAtTail(head , 25);
+    insertAtTail(head , 26);
+    insertAtTail(head , 27);
     display(head);
 
-    insertAtHead(head,10);
+
+
+    insertAtHead(head, 10);
     display(head);
 
-    cout<<Search(head, 20);
+    cout<<search(head, 23);
+
+
 }
