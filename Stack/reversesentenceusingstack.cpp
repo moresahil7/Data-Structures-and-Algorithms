f36adff4db2c37c51c91d9b2c59b18e28ef315c4
Sahil <moresahil4502@gmail.com>
2021-10-31 10:31:47 +0530

    reversed a string using stack

--- Stack/reversesentenceusingstack.cpp
+++ Stack/reversesentenceusingstack.cpp
@@ -1,1 +1,27 @@
-#include<iostre
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void reverseAsentence(string s){
+    stack<string> st;
+
+    for(int i = 0; i<s.length() ; i++){
+        string word = "";
+
+        while(s[i]!=' ' && i<s.length()){
+            word = word+s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
+
+int main(){
+    string s = "hey, Hello how are you?";
+    reverseAsentence(s);
+}
