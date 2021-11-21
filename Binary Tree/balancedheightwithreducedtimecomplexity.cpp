146725939b9244021b3799aa169cf26c55493e20
Sahil <moresahil4502@gmail.com>
2021-11-21 13:41:35 +0530

    balanced height with reduced time complexity

--- Binary Tree/balancedheightwithreducedtimecomplexity.cpp
+++ Binary Tree/balancedheightwithreducedtimecomplexity.cpp
@@ -18,36 +18,30 @@ struct Node{
     }
 };
 
-int height(Node* root){
-    if(root == NULL){
-        return -1;
-    }
-
-        int lh = height(root->left);
-        int rh = height(root->right);
-
-        return max(lh,rh)+1;
-    
-}
 
 
 
 
-bool isBalanced(Node* root){
+
+bool isBalanced(Node* root, int* height){
     if(root == NULL){
         return true;
     }
 
-    if(isBalanced(root->left) == false){
+    int lh=0;
+    int rh=0;
+
+    if(isBalanced(root->left,&lh) == false){
         return false;
     }
-    if(isBalanced(root->right) == false){
+    if(isBalanced(root->right,&rh) == false){
         return false;
     }
 
+    *height = max(lh,rh) + 1;
 
-    int lh = height(root->left);
-    int rh = height(root->right);
+
+    
 
     if(abs(lh-rh) <= 1){
         return true;
@@ -76,21 +70,25 @@ int main(){
     struct Node* root = new Node(1);
 
     root->left = new Node(2);
-    root->right = new Node(3);
+    // root->right = new Node(3);
     root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    // root->left->right = new Node(5);
+    // root->right->left = new Node(6);
+    // root->right->right = new Node(7);
+    int height = 0;
 
-    preorder(root);
-    cout<<endl;
-    cout<<isBalanced(root)<<endl;
+    if(isBalanced(root,&height) == true){
+        cout<<"balanced tree"<<endl;
+    }
+    else{
+        cout<<"Not Balanced"<<endl;
+    }
    
 
     
-
+return 0;
 }
 
 
-//Time complexity ::::  O(n^2)
+//Time complexity ::::  O(n)
 
