2b984bfdb24a58eb4f8423542bd03cf5c2c19a71
Sahil <moresahil4502@gmail.com>
2021-11-22 20:37:50 +0530

    dist between 2 nodes of binary tree

--- Binary Tree/leftviewofBinarytree.cpp
+++ Binary Tree/leftviewofBinarytree.cpp
@@ -49,7 +49,7 @@ while(!q.empty()){
 int main(){
     Node* root = new Node(1);
     root->left = new Node(2);
-    root->right = new Node(3);
+    root->right = new Node(3); 
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     root->right->left = new Node(6);
