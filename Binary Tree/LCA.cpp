c9450902f0936f7b82e38de27cdd3ace0ff39135
Sahil <moresahil4502@gmail.com>
2021-12-14 10:40:40 +0530

    LCA by recursion

--- Binary Tree/LCA.cpp
+++ Binary Tree/LCA.cpp
@@ -14,45 +14,68 @@ struct Node{
     }
 };
 
-bool getPath(Node *root , int key, vector<int> &path){
+// bool getPath(Node *root , int key, vector<int> &path){
+//     if(root == NULL){
+//         return false;
+//     }
+
+//     path.push_back(root->data);
+
+//     if(root->data == key){
+//         return true;
+//     }
+
+
+//     if(getPath(root->left,key ,path) || getPath(root->right,key,path)){
+//         return true;
+//     }
+
+
+//     path.pop_back();
+//     return false;
+// }
+
+
+// int LCA(Node* root, int n1, int n2){
+//     vector<int> path1,path2;
+
+//     if(!getPath(root , n1 , path1) || !getPath(root ,n2 , path2)){
+//         return -1;
+//     }
+
+
+//     int pc;
+//     for(pc = 0; pc<path1.size() && path2.size();pc++){
+//         if(path1[pc] != path2[pc]){
+//             break;
+//         }
+//     }
+
+//     return path1[pc-1];
+
+// }
+
+
+Node* lcaRecur(Node* root, int n1, int n2){
     if(root == NULL){
-        return false;
+        return NULL;
     }
 
-    path.push_back(root->data);
-
-    if(root->data == key){
-        return true;
+    if(root->data == n1 || root->data == n2){
+        return root;
     }
 
+    Node* leftLCA = lcaRecur(root->left, n1, n2);
+    Node* rightLCA = lcaRecur(root->right, n1, n2);
 
-    if(getPath(root->left,key ,path) || getPath(root->right,key,path)){
-        return true;
+    if(leftLCA && rightLCA){
+        return root;
     }
 
-
-    path.pop_back();
-    return false;
-}
-
-
-int LCA(Node* root, int n1, int n2){
-    vector<int> path1,path2;
-
-    if(!getPath(root , n1 , path1) || !getPath(root ,n2 , path2)){
-        return -1;
+    if(leftLCA != NULL){
+        return leftLCA;
     }
-
-
-    int pc;
-    for(pc = 0; pc<path1.size() && path2.size();pc++){
-        if(path1[pc] != path2[pc]){
-            break;
-        }
-    }
-
-    return path1[pc-1];
-
+    return rightLCA;
 }
 
 int main(){
@@ -64,6 +87,10 @@ int main(){
     root->right->left = new Node(6);
     root->right->right = new Node(7);
 
-    cout<<LCA(root,6,7);
+    // cout<<LCA(root,6,7);
+    // Node* LCA = lcaRecur(root,4,5);
+
+    cout<<lcaRecur(root,4,5)->data;
+
 
 }
